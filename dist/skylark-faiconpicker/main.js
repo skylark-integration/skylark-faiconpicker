81a16edc31a7819daf7e2f643613d9b0925f2326
hudaokeji <github@hudaokeji.com>
2019-11-08 23:31:42 +0900

    init commit

---
+++ dist/skylark-faiconpicker/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-faiconpicker - A version of fontawesome-iconpicker that ported to running on skylarkjs
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-integration/skylark-faiconpicker/
+ * @license MIT
+ */
+define(["./IconPicker"],function(n){return n});
+//# sourceMappingURL=sourcemaps/main.js.map
